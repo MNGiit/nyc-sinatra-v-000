b091699641a24d8f2d5db18e502f3e598ea4bb8b
Mohammed Noori <kmanoorib@gmail.com>
2019-05-29 10:45:15 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,18 +15,3 @@ class LandmarksController < ApplicationController
   end
   
 end
-
-  post '/figures' do
-    # binding.pry
-    @figure = Figure.create(params[:figure])
-    if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(params[:landmark])
-    end
-    
-    if !params[:title][:name].empty?
-      @figure.titles << Title.create(params[:title])
-    end
-    @figure.save
-    
-    redirect to "/figures/#{@figure.id}"
-  end
