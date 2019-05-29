5d14d184106898977541a79609ceb6a810199986
Mohammed Noori <kmanoorib@gmail.com>
2019-05-29 11:01:36 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -35,18 +35,3 @@ class LandmarksController < ApplicationController
     redirect to "/landmarks/#{@landmark.id}" # redirect to landmarks/:id
   end
 end
-
-  patch '/figures/:id' do
-    @figure = Figure.find_by_id(params[:id]) # find figure
-    @figure.update(params[:figure]) # update figure
-    if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(params[:landmark])
-    end
-    
-    if !params[:title][:name].empty?
-      @figure.titles << Title.create(params[:title])
-    end
-    @figure.save
-    
-    redirect to "/figures/#{@figure.id}" # redirect to figure/:id
-  end
